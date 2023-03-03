1ab5e2b62ff6c22a4ce38447a494496fe532f1d4
Jefferson Bruno <bruunosoousaa@gmail.com>
2023-03-03 09:22:15 -0300

    cobertura de testes tela de login

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,4 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
 
-
+describe('Teste tela de Login', () => {
+  test('Testa se elementos existem na tela de Login', () => {
+    render(<App />);
+    const email = screen.getByTestId('email-input');
+    const password = screen.getByTestId('password-input');
+    const submit = screen.getByTestId('login-submit-btn');
+    expect(email).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    expect(submit).toBeInTheDocument();
+  });
+  test('Testa de input na tela de Login', () => {
+    render(<App />);
+    const email = screen.getByTestId('email-input');
+    const password = screen.getByTestId('password-input');
+    const submit = screen.getByTestId('login-submit-btn');
+    userEvent.type(email, 'email@mail.com');
+    userEvent.type(password, '1234567');
+    expect(submit).toBeEnabled();
+    userEvent.click(submit);
+  });
+});
