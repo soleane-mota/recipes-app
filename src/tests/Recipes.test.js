c39a857dc5b0a4173b53581f761e35f7272af4b5
Jessica <je.proenca@yahoo.com.br>
2023-03-03 21:47:04 -0300

    testes...

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import meals from '../../cypress/mocks/meals';
-import Meals from '../pages/Meals';
+import drinks from '../../cypress/mocks/drinks';
+import RecipesProvider from '../Context/RecipesProvider';
 
 beforeEach(() => {
   jest.spyOn(global, 'fetch').mockResolvedValue(
     { json: jest.fn().mockResolvedValue(meals) },
+  );
+});
+
+beforeEach(() => {
+  jest.spyOn(global, 'fetch').mockResolvedValue(
+    { json: jest.fn().mockResolvedValue(drinks) },
   );
 });
 
@@ -16,7 +23,27 @@ test('Verifica se a API é chamada', () => {
   expect(global.fetch).toHaveBeenCalledTimes(4);
 });
 
-// test('Verifica se aparecem 12 comidas na tela', () => {
-//   render(<Meals />);
-//   expect(screen.getByText('Meals')).toBeInTheDocument();
-// });
+test('Verifica se aparecem 12 comidas na tela', async () => {
+  render(<App />);
+  const email = screen.getByTestId('email-input');
+  const password = screen.getByTestId('password-input');
+  const submit = screen.getByTestId('login-submit-btn');
+  userEvent.type(email, 'email@mail.com');
+  userEvent.type(password, '1234567');
+  expect(submit).toBeEnabled();
+  userEvent.click(submit);
+});
+
+it('Verifica se possui um ícone que redireciona para tela Drinks', async () => {
+  render(<RecipesProvider><App /></RecipesProvider>, ['/meals']);
+
+  await waitFor(() => {
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  const btnDrinks = screen.getAllByRole('button');
+  expect(btnDrinks).toHaveLength(6);
+
+  const carregando = screen.getByText('Corba');
+  expect(carregando).toBeInTheDocument();
+}, 30000);
